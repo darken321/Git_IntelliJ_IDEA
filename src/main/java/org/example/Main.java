343837b7639e81b87038248e1c8787f33d87abfa
Vasili Bandarenka <darken123@inbox.ru>
2022-10-21 12:56:15 +0300

    added multiplication

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,8 +6,12 @@ public class Main {
         int aa=2;
         int bb=5;
         System.out.println("сумма " + aa + " + " + bb + " = " +  plus (2,5));
+        System.out.println("произведение " + aa + " Х " + bb + " = " +  multiplication (2,5));
     }
     static int plus(int a, int b){
         return a+b;
+    }
+    static int multiplication (int a, int b){
+        return a*b;
     }
  }
