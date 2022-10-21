425ff98f4aaa1e4ead6fe4c03485a1121e67953d
Vasili Bandarenka <darken123@inbox.ru>
2022-10-21 14:43:18 +0300

    added hello

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,14 +4,20 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
         int aa=2;
-        int bb=5;
-        System.out.println("сумма " + aa + " + " + bb + " = " +  plus (2,5));
-        System.out.println("произведение " + aa + " Х " + bb + " = " +  multiplication (2,5));
+        int bb = 5;
+        System.out.println("сумма " + aa + " + " + bb + " = " + plus(2, 5));
+        System.out.println("произведение " + aa + " Х " + bb + " = " + multiplication(2, 5));
     }
-    static int plus(int a, int b){
-        return a+b;
+
+    static int plus(int a, int b) {
+        return a + b;
     }
-    static int multiplication (int a, int b){
-        return a*b;
+
+    static int multiplication(int a, int b) {
+        return a * b;
     }
- }
+
+    static void hello() {
+        System.out.println("hello!!");
+    }
+}
