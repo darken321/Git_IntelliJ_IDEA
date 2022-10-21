bb4d617366fca5bf22ff4bc4b57ed78adb63b360
Vasili Bandarenka <darken123@inbox.ru>
2022-10-21 12:46:25 +0300

    added summ

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,5 +3,11 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int aa=2;
+        int bb=5;
+        System.out.println("сумма " + aa + " + " + bb + " = " +  plus (2,5));
+    }
+    static int plus(int a, int b){
+        return a+b;
     }
  }
